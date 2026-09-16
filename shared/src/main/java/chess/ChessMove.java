8247b993e4b69ce2f03438e5296dbd93502ad649
Caleb Douglass <calebkd@byu.edu>
2026-09-16 09:43:41 -0600

    fix all broke by add ChessMove equals/hash

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -49,5 +51,19 @@ public class ChessMove {
             return String.format("[%s>%s]", startPosition, endPosition);
         }
         return String.format("[%s>%s,^%s]",startPosition, endPosition, promotionPiece);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
     }
 }
